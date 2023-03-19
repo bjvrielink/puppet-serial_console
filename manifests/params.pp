9a4f2535ade194647197a92fbb5303d368b260f1
Bart-Jan Vrielink <bartjan@vrielink.net>
2023-03-19 20:11:51 +0100

    Add support for 9

--- manifests/params.pp
+++ manifests/params.pp
@@ -29,7 +29,7 @@ class serial_console::params {
           $cmd_refresh_bootloader = undef
         }
 
-        '7', '8': {
+        '7', '8', "9": {
           $class_kernel = 'grub2'
           $class_bootloader = 'grub2'
           $class_getty = undef
